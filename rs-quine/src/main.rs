fd5d26b4ec52f31bf7c3f611a1d0ec58b6e76c6c
jacobwshg <wsg2002@hotmail.com>
2025-07-15 23:13:31 -0400

    Rust: can print boilerplate and src string, except quotes not escaped as in cat output

--- rs-quine/src/main.rs
+++ rs-quine/src/main.rs
@@ -3,12 +3,24 @@ fn main() {
         "fn main() {{",
         "    let src = [",
         "    ];",
-        "    for lineidx in 0..src.len() {{",
-        "        println!(\"{{}}\", src[lineidx]);",
+        "    for lineidx in 0..2 {",
+        "        println!(\"{}\", src[lineidx]);",
+        "    }",
+        "    for lineidx in 0..src.len() {",
+        "        println!(\"        {cm}{ln}{cm},\", cm = 0x22 as char, ln = src[lineidx]);",
+        "    }}",
+        "    for lineidx in 2..src.len() {{",
+        "        println!(\"{}\", src[lineidx]);",
         "    }}",
         "}}",
     ];
+    for lineidx in 0..2 {
+        println!("{}", src[lineidx]);
+    }
     for lineidx in 0..src.len() {
+        println!("        {cm}{ln}{cm},", cm = 0x22 as char, ln = src[lineidx]);
+    }
+    for lineidx in 2..src.len() {
         println!("{}", src[lineidx]);
     }
 }
